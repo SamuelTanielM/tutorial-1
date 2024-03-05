e04f67cc181cac3b1148ca78ac126c9fad0fc6cd
Samuel Taniel Mulyadi <samueltaniel@gmail.com>
2024-03-05 09:04:12 +0700

    [GREEN] Implement Order model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,2 +1,26 @@
-package id.ac.ui.cs.advprog.eshop.model;public class Order {
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Builder
+@Getter
+public class Order {
+    private String id;
+    private List<Product> products;
+    private long orderTime;
+    private String author;
+
+    @Setter
+    String status;
+
+    // Constructor
+    public Order(String id, List<Product> products, long orderTime, String author) {
+    }
+
+    public Order(String id, List<Product> products, long orderTime, String author, String status) {
+    }
 }
