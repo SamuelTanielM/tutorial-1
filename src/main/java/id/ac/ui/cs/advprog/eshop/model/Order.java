16d8c1de7bfbfc41410a2baabe3cf03823924a74
Samuel Taniel Mulyadi <samueltaniel@gmail.com>
2024-03-05 09:06:09 +0700

    [REFACTOR] Add OrderStatus enum.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -4,23 +4,47 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Builder
 @Getter
 public class Order {
-    private String id;
-    private List<Product> products;
-    private long orderTime;
-    private String author;
-
-    @Setter
+    String id;
+    List<Product> products;
+    Long orderTime;
+    String author;
     String status;
 
     // Constructor
-    public Order(String id, List<Product> products, long orderTime, String author) {
+    public Order(String id, List<Product> products, Long orderTime, String author) {
+        this.id = id;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = "WAITING_PAYMENT";
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.products = products;
+        }
     }
 
-    public Order(String id, List<Product> products, long orderTime, String author, String status) {
+    public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+        this(id, products, orderTime, author);
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+    }
+
+    public void setStatus(String status) {
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
