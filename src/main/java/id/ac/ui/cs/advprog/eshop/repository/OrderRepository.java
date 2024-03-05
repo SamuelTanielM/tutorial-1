7e7f0308d99ddbd2bd9fe9182a283dd26264e948
Samuel Taniel Mulyadi <samueltaniel@gmail.com>
2024-03-05 09:32:35 +0700

    [GREEN] Implement OrderRepository class.

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -1,2 +1,28 @@
-package id.ac.ui.cs.advprog.eshop.repository;public class OrderRepository {
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class OrderRepository {
+    private List<Order> orderData = new ArrayList<>();
+
+    public Order save(Order order) {
+        // Implementation goes here
+        return order;
+    }
+
+    public Order findById(String id) {
+        // Implementation goes here
+        return null;
+    }
+
+    public List<Order> findAllByAuthor(String author) {
+        // Implementation goes here
+        return null;
+    }
 }
+
