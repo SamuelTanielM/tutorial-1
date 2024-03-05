6efe5ad5f4e83f0f787cb34be3c6ed2fd581de5c
Samuel Taniel Mulyadi <samueltaniel@gmail.com>
2024-03-05 10:18:51 +0700

    [GREEN] Implement OrderRepository class.

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
@@ -1,2 +1,12 @@
-package id.ac.ui.cs.advprog.eshop.service;public class OrderService {
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+
+import java.util.List;
+
+public interface OrderService {
+    public Order createOrder(Order order);
+    public Order updateStatus(String orderId, String status);
+    public Order findById(String orderId);
+    public List<Order> findAllByAuthor(String author);
 }
